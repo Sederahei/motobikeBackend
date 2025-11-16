99cd8d3af23f933f7a164a0a825bfb2cbbe1903a
sederahei <hei.sedera.167@gmail.com>
2025-11-16 13:07:32 +0300

    feat: creat for clients

--- src/main/java/dev/sedera/motobike/repository/PanierRepository.java
+++ src/main/java/dev/sedera/motobike/repository/PanierRepository.java
@@ -1,4 +1,12 @@
 package dev.sedera.motobike.repository;
 
-public class PanierRepository {
+import dev.sedera.motobike.entity.Panier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PanierRepository extends JpaRepository<Panier, Long> {
+    // Permet de retrouver le panier d’un client
+    Panier findByClientId(Long clientId);
 }
+
