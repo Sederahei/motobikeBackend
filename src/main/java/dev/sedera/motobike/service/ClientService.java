b244bbfee00b3c034c2cd75c4b4267156f1c0f21
sederahei <hei.sedera.167@gmail.com>
2025-11-28 18:52:42 +0300

    refact: add new asswordEncoder

--- src/main/java/dev/sedera/motobike/service/ClientService.java
+++ src/main/java/dev/sedera/motobike/service/ClientService.java
@@ -1,7 +1,8 @@
 package dev.sedera.motobike.service;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import dev.sedera.motobike.entity.Client;
 import dev.sedera.motobike.repository.ClientRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,25 +11,23 @@ import java.util.List;
 public class ClientService {
 
     private final ClientRepository clientRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public ClientService(ClientRepository clientRepository) {
+    public ClientService(ClientRepository clientRepository, PasswordEncoder passwordEncoder) {
         this.clientRepository = clientRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<Client> getAllClients() {
-
-
         return clientRepository.findAll();
     }
 
     public Client getClientById(Long id) {
-
         return clientRepository.findById(id)
-                .orElseThrow(()->new RuntimeException("id introuvable avec cette id  :" + id));
+                .orElseThrow(() -> new RuntimeException("id introuvable avec cette id :" + id));
     }
 
     public Client getClientByEmail(String email) {
-
         return clientRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("Client introuvable avec email: " + email));
     }
@@ -41,13 +40,12 @@ public class ClientService {
         return clientRepository.findByTelephoneContainingIgnoreCase(telephone);
     }
 
-
     public List<Client> getClientsByNom(String nom) {
         return clientRepository.findByNomContainingIgnoreCase(nom);
     }
 
-
     public Client saveClient(Client client) {
+        client.setMotDePasse(passwordEncoder.encode(client.getMotDePasse()));
         return clientRepository.save(client);
     }
 
